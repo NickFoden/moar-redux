35e58161d80127d17f00044b7bc2aded9078eacf
Nick Foden <nickfoden@gmail.com>
2017-10-09 20:55:06 -0400

    Removed connect from app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import ToDoForm from './components/ToDoForm';
 import ToDoList from './components/ToDoList';
-import {updateCurrent} from './reducers/todo'
 
 
 class App extends Component {
@@ -16,18 +14,12 @@ class App extends Component {
           <h1 className="App-title">Welcome to React with Redux</h1>
         </header>
         <div className="Todo-App">
-          <ToDoForm 
-          currentTodo={this.props.currentTodo} 
-          changeCurrent={this.props.updateCurrent}
-          />
-          <ToDoList todos={this.props.todos} />
+          <ToDoForm />
+          <ToDoList />
         </div>
       </div>
     );
   }
 }
  
-export default connect(
-  (state) => state,
-  {updateCurrent}
-)(App)
+export default App
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux'
 
 const ToDoItem = ({id, name, isComplete}) => (
     <li>
@@ -7,7 +8,7 @@ const ToDoItem = ({id, name, isComplete}) => (
     </li>
 )
 
-export default (props) => (
+const TodoList = (props) => (
     <div className="Todo-List">
         <ul>
             {props.todos.map(todo => <ToDoItem key={todo.id} {...todo}/>)}
@@ -15,3 +16,7 @@ export default (props) => (
   </div>
 
 )
+
+export default connect(
+    (state) => ({todos:state.todos})
+)(TodoList)
